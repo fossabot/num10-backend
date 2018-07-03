c490fa5a33e042e7a4c9d9f264714bf5e99bc811
phongduong <s2pkmol@gmail.com>
2018-07-03 22:08:44 +0700

    Update package

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-import express from ''
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 4000
+
+app.get("/", (req, res) => {
+  return res.json({ a: "Hello" });
+});
+
+app.listen(port, () => console.log(`Running on port ${port}`));
