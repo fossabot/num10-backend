6fa328973df6635ac395addbfe8f05e66811b125
phongduong <phongduong1710@gmail.com>
2018-08-02 15:28:25 +0700

    test

--- api.js
+++ api.js
@@ -1,7 +1,9 @@
-const app = require('express')
-const router = app.Router()
-const {auth} = require("./routers/auth")
+const app = require("express");
+const router = app.Router();
+const { auth } = require("./routers/auth");
 
-router.use("/auth", auth)
+router.use("/auth", auth);
 
-exports.router = router
+router.get("/test", (req, res) => res.json({ test: "ok" }));
+
+exports.router = router;
