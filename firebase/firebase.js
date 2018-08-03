81b71dfd0cbe209daef5dc6ab1cc2bc2ef8cc1b8
phongduong <phongduong1710@gmail.com>
2018-08-03 10:03:48 +0700

    Update firebase credential

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,27 +1,8 @@
 const admin = require("firebase-admin");
-const {
-  PROJECT_ID,
-  CLIENT_EMAIL,
-  PRIVATE_KEY_ID,
-  PRIVATE_KEY,
-  DATABASE_NAME,
-  CLIENT_ID
-} = require("../constants");
+const { DATABASE_NAME, FIREBASE_CREDENTIAL } = require("../constants");
 
 admin.initializeApp({
-  credential: admin.credential.cert({
-    type: "service_account",
-    project_id: PROJECT_ID,
-    private_key_id: PRIVATE_KEY_ID,
-    private_key: JSON.parse(PRIVATE_KEY),
-    client_email: CLIENT_EMAIL,
-    client_id: CLIENT_ID,
-    auth_uri: "https://accounts.google.com/o/oauth2/auth",
-    token_uri: "https://accounts.google.com/o/oauth2/token",
-    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-    client_x509_cert_url:
-      "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-n5j69%40num10-208508.iam.gserviceaccount.com"
-  }),
+  credential: admin.credential.cert(JSON.parse(FIREBASE_CREDENTIAL)),
   databaseURL: `https://${DATABASE_NAME}.firebaseio.com`
 });
 
